4ee7c8889c60a633e0f65ef042bb92a11ff8ce53
Alex Code <alex.for.code@gmail.com>
2021-03-16 10:23:24 +0300

    Problems: 153

---
+++ 153.py
@@ -0,0 +1,40 @@
+"""
+Suppose an array of length n sorted in ascending order is rotated between 1 and n times.
+For example, the array nums = [0,1,2,4,5,6,7] might become:
+
+    [4,5,6,7,0,1,2] if it was rotated 4 times.
+    [0,1,2,4,5,6,7] if it was rotated 7 times.
+
+Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time
+results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+
+Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+
+Constraints:
+    n == nums.length
+    1 <= n <= 5000
+    -5000 <= nums[i] <= 5000
+    All the integers of nums are unique.
+    nums is sorted and rotated between 1 and n times.
+"""
+from typing import List
+
+
+def find_min(nums: List[int]) -> int:
+    start: int = 0
+    end: int = len(nums) - 1
+
+    while start < end:
+        mid: int = start + (end - start) // 2
+        if nums[mid] > nums[end]:
+            start = mid + 1
+        else:
+            end = mid
+
+    return nums[start]
+
+
+if __name__ == '__main__':
+    assert find_min([3, 4, 5, 1, 2]) == 1
+    assert find_min([4, 5, 6, 7, 0, 1, 2]) == 0
+    assert find_min([11, 13, 15, 17]) == 11
