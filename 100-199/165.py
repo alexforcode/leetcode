be1452cd5a95262a57e2599ad6fe84d209d55807
Alex Code <alex.for.code@gmail.com>
2021-05-21 08:14:46 +0300

    Problem: 165

---
+++ 100-199/165.py
@@ -0,0 +1,48 @@
+"""
+Given two version numbers, version1 and version2, compare them.
+
+Version numbers consist of one or more revisions joined by a dot '.'. Each revision consists of digits and may contain
+leading zeros. Every revision contains at least one character. Revisions are 0-indexed from left to right,
+with the leftmost revision being revision 0, the next revision being revision 1, and so on.
+For example 2.5.33 and 0.1 are valid version numbers.
+
+To compare version numbers, compare their revisions in left-to-right order. Revisions are compared using their integer
+value ignoring any leading zeros. This means that revisions 1 and 001 are considered equal.
+If a version number does not specify a revision at an index, then treat the revision as 0.
+For example, version 1.0 is less than version 1.1 because their revision 0s are the same, but their
+revision 1s are 0 and 1 respectively, and 0 < 1.
+
+Return the following:
+    If version1 < version2, return -1.
+    If version1 > version2, return 1.
+    Otherwise, return 0.
+
+Constraints:
+    1 <= version1.length, version2.length <= 500
+    version1 and version2 only contain digits and '.'.
+    version1 and version2 are valid version numbers.
+    All the given revisions in version1 and version2 can be stored in a 32-bit integer.
+"""
+from itertools import zip_longest
+from typing import List
+
+
+def compare_version(version1: str, version2: str) -> int:
+    v1: List[int] = [int(v) for v in version1.split('.')]
+    v2: List[int] = [int(v) for v in version2.split('.')]
+
+    for val1, val2 in zip_longest(v1, v2, fillvalue=0):
+        if val1 < val2:
+            return -1
+        elif val1 > val2:
+            return 1
+
+    return 0
+
+
+if __name__ == '__main__':
+    assert compare_version('1.01', '1.001') == 0
+    assert compare_version('1.0', '1.0.0') == 0
+    assert compare_version('0.1', '1.1') == -1
+    assert compare_version('1.0.1', '1') == 1
+    assert compare_version('7.5.2.4', '7.5.3') == -1
